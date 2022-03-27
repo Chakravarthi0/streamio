d7cb9d999ede75b8c0005d43662119315b54c0c8
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-03-28 02:23:24 +0530

    feat: added navbar and aside navbar

---
+++ src/layouts/navbar/NavBar.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { useLocation, Link } from "react-router-dom";
+import "./navbar.css";
+function NavBar() {
+  const { pathname } = useLocation();
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setIsNavOpen(false);
+  }, [pathname]);
+
+  const toggleIsOpen = () => {
+    setIsNavOpen((prev) => !prev);
+  };
+
+  return (
+    <header className="navbar-container bg-primary">
+      <Link to={"/"} className="link">
+        <h2 className="nav-logo">Streamio</h2>
+      </Link>
+
+      <div className="nav-search-container nav-search icon">
+        <i className="fa fa-search" aria-hidden="true"></i>
+        <input className="nav-search-input" placeholder="Search" />
+      </div>
+
+      <nav>
+        <ul className={"list nav-links-container"}>
+          <li>
+            <Link className="link nav-link" to={"/signin"}>
+              Sign In
+            </Link>
+          </li>
+        </ul>
+        <i className={"material-icons hamburger-icon"} onClick={toggleIsOpen}>
+          {isNavOpen ? "close" : "menu"}
+        </i>
+      </nav>
+    </header>
+  );
+}
+export {NavBar};
