3a0f659351b4cf60e5903fee82844651d2fceb9c
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-03-28 17:46:17 +0530

    feat: implemented categories context in homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Chips, VideoCard, Loader, LoadingError } from "../../components/index";
-import { categories } from "../../backend/db/categories";
-import { useVideos } from "../../context/index";
+import { useVideos, useCategory } from "../../context/index";
 import "./home.css";
 
 function Home() {
   const { error, videos, isLoading } = useVideos();
+  const { categories } = useCategory();
+
   return (
     <div className="homepage-container">
       <div className="category-list-container">
         {categories.map((ele) => (
-          <Chips>{ele.categoryName}</Chips>
+          <Chips key={ele._id}>{ele.categoryName}</Chips>
         ))}
       </div>
 
