48155c86d8274467dc5ca30498fb81aa56eea7ed
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-03-30 00:46:22 +0530

    feat: added history context

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,4 @@
 export { VideosProvider, useVideos } from "./VideosContext";
 export { CategoryProvider, useCategory } from "./CategoryContext";
-export {AuthProvider, useAuth} from "./authContext"
+export { AuthProvider, useAuth } from "./authContext";
+export { HistoryProvider, useHistory } from "./HistoryContext";
