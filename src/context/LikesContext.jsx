aa2aea9f70be57803cd6f7081d7d7715ec45e2c1
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-03-31 02:05:28 +0530

    feat: added likes context

---
+++ src/context/LikesContext.jsx
@@ -0,0 +1,93 @@
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
+import { likesActions, likesReducer } from "../reducers/index";
+import { useAuth } from "./index";
+
+const likesContext = createContext([]);
+
+const useLikes = () => useContext(likesContext);
+
+function LikesProvider({ children }) {
+  const {
+    authState: { token },
+  } = useAuth();
+
+  const [likesState, likesDispatch] = useReducer(likesReducer, {
+    likes: [],
+    error: false,
+    loading: false,
+  });
+  const { SET_LIKES, SET_LOADING, SET_ERROR } = likesActions;
+
+  useEffect(() => {
+    (async () => {
+      if (token) {
+        try {
+          likesDispatch({ type: SET_ERROR, payload: false });
+          likesDispatch({ type: SET_LOADING, payload: true });
+          let response = await axios.get("/api/user/likes", {
+            headers: {
+              authorization: token,
+            },
+          });
+          if (response.status === 200) {
+            likesDispatch({ type: SET_LIKES, payload: response.data.likes });
+          }
+        } catch (err) {
+          likesDispatch({ type: SET_ERROR, payload: true });
+          console.log(err);
+        } finally {
+          likesDispatch({ type: SET_LOADING, payload: false });
+        }
+      }
+    })();
+  }, [token]);
+
+  const addToLikes = async (video) => {
+    try {
+      likesDispatch({ type: SET_ERROR, payload: false });
+      const response = await axios.post(
+        "/api/user/likes",
+        { video },
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+
+      if (response.status === 201) {
+        likesDispatch({ type: SET_LIKES, payload: response.data.likes });
+      }
+    } catch (err) {
+      likesDispatch({ type: SET_ERROR, payload: true });
+      console.log(err);
+    }
+  };
+
+  const removeFromLikes = async (videoId) => {
+    try {
+      likesDispatch({ type: SET_ERROR, payload: false });
+      const response = await axios.delete(`/api/user/likes/${videoId}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+
+      if (response.status === 200) {
+        likesDispatch({ type: SET_LIKES, payload: response.data.likes });
+      }
+    } catch (err) {
+      likesDispatch({ type: SET_ERROR, payload: true });
+      console.log(err);
+    }
+  };
+
+  return (
+    <likesContext.Provider value={{ likesState, addToLikes, removeFromLikes }}>
+      {children}
+    </likesContext.Provider>
+  );
+}
+
+export { LikesProvider, useLikes };
--- src/context/index.js
+++ src/context/index.js
@@ -2,3 +2,4 @@ export { VideosProvider, useVideos } from "./VideosContext";
 export { CategoryProvider, useCategory } from "./CategoryContext";
 export { AuthProvider, useAuth } from "./authContext";
 export { HistoryProvider, useHistory } from "./HistoryContext";
+export { LikesProvider, useLikes } from "./LikesContext";
