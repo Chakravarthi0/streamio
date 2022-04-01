95c268690bc7fd6ecd15dc3f84eab3e05fd0d885
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-04-02 03:08:13 +0530

    feat: implemented watch later

--- src/pages/watch-later/WatchLater.jsx
+++ src/pages/watch-later/WatchLater.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-import { VideoCard } from "../../components/index";
+import { VideoCard, LoadingError, Loader } from "../../components/index";
 import { videos } from "../../backend/db/videos";
+import { useWatchLater } from "../../context/index";
 import "./watch-later.css";
 
 function WatchLater() {
+  const {
+    watchLaterState: { watchLater, loading, error },
+    addToWatchLater,
+    removeFromWatchLater,
+  } = useWatchLater();
   return (
     <div>
-        <h1 className="page-title text-center">Watch later</h1>
+      <h1 className="page-title text-center">Watch later</h1>
       <div className="videos-container">
-        {videos.map((video) => (
-          <VideoCard key={video._id} video={video} />
-        ))}
+        {loading && <Loader isFullScreen={true} />}
+
+        {error && <LoadingError />}
+
+        {watchLater.length > 0 &&
+          watchLater.map((video) => (
+            <VideoCard key={video._id} video={video} />
+          ))}
       </div>
+      {watchLater.length === 0 && !error && (
+        <h2 className="text-center">No liked videos</h2>
+      )}
     </div>
   );
 }
