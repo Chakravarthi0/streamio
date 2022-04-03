eadf332450c50924a98d8f01c6fa17d7a2ae5e35
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-04-03 07:24:09 +0530

    refactor: refactored app layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { NavBar, AsideNavBar, ScrollToTop, Footer } from "./layouts/index";
-import AppRoutes from "./app-routes/AppRoutes"
+import AppRoutes from "./app-routes/AppRoutes";
+import { useLocation } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const { pathname } = useLocation();
+  const sidebarRestrictedRoutes = ["/signin", "/signup", "/"];
   return (
     <div className="App">
       <NavBar />
-      <div className="main-container">
-        <AsideNavBar />
-        <AppRoutes/>
+      <div
+        className={
+          sidebarRestrictedRoutes.includes(pathname) ? "" : "main-container"
+        }
+      >
+        {!sidebarRestrictedRoutes.includes(pathname) && <AsideNavBar />}
+        <AppRoutes />
       </div>
-        <ScrollToTop />
+      {!sidebarRestrictedRoutes.includes(pathname) && <ScrollToTop />}
       <Footer />
     </div>
   );
