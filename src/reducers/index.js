648272def10aa6bfc9b6e719ca2e42dadc161344
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-04-03 07:25:55 +0530

    feat: added filter context

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,10 @@ export {
   likesActions,
   playlistActions,
   watchLaterActions,
+  filtersActions,
 } from "./actionTypes";
 export { authReducer } from "./authReducer";
 export { likesReducer } from "./likesReducer";
 export { playlistReducer } from "./playlistReducer";
 export { watchLaterReducer } from "./watchLaterReducer";
+export { filtersReducer } from "./filtersReducer";
