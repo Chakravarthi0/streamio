eec13535ccc9baa1a16f81f11a2b1e927ae18729
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-04-03 07:27:24 +0530

    feat: implemented filter

--- src/components/chips/Chips.jsx
+++ src/components/chips/Chips.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import "./chips.css"
+import React from "react";
+import { useFilters } from "../../context/index";
+import { filtersActions } from "../../reducers/index";
+import { useNavigate } from "react-router-dom";
+import "./chips.css";
 
-function Chips({children}) {
+function Chips({ children, name, isFromHome }) {
+  const navigate = useNavigate();
+  const {
+    filtersState: { category },
+    filtersDispatch,
+  } = useFilters();
   return (
-    <span className='chips-container'>{children}</span>
-  )
+    <span
+      className={
+        `chips-container ` +
+        (!isFromHome && category === name ? "chip-active" : "")
+      }
+      onClick={() => {
+        if (isFromHome) {
+          filtersDispatch({ type: filtersActions.RESET_FILTERS });
+          navigate("/explore");
+        }
+        filtersDispatch({ type: filtersActions.SET_CATEGORY, payload: name });
+      }}
+    >
+      {children}
+    </span>
+  );
 }
 
-export {Chips};
+export { Chips };
--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,26 +1,101 @@
 import React from "react";
-import { VideoCard, Loader, LoadingError } from "../../components/index";
-import { useVideos } from "../../context/index";
+import {
+  VideoCard,
+  Loader,
+  LoadingError,
+  Chips,
+  SearchBar,
+} from "../../components/index";
+import { useVideos, useCategory, useFilters } from "../../context/index";
+import { filtersActions } from "../../reducers/index";
 import "./explore.css";
 
 function Explore() {
+  const { categories } = useCategory();
   const { videos, isLoading, error } = useVideos();
+
+  const {
+    filtersState: { sortBy, category, searchKey },
+    filtersDispatch,
+  } = useFilters();
+  const { SET_SORT } = filtersActions;
+
+  const filterVideos = () => {
+    let res = videos.slice();
+    if (category !== "") {
+      res = res.filter((ele) => ele.catgeory === category);
+    }
+
+    if (searchKey !== "") {
+      res = res.filter(
+        (ele) =>
+          ele.title.toLowerCase().includes(searchKey.toLowerCase()) ||
+          ele.channel.toLowerCase().includes(searchKey.toLowerCase())
+      );
+    }
+    if(sortBy === "ascending"){
+      res = res.sort((a,b) => new Date(b.uploadDate) - new Date(a.uploadDate))
+      console.log("des After sort...", res)
+    }else  if(sortBy === "descending"){
+      res = res.sort((a,b) => new Date(a.uploadDate) - new Date(b.uploadDate))
+      console.log("asc After sort...", res)
+    }
+    return res;
+  };
+
+  const filteredVideos = filterVideos();
+
   return (
     <div className="explore-container">
       {isLoading && <Loader />}
       {error && <LoadingError />}
 
-      {videos.length > 0 && (
+      {!isLoading && <SearchBar fromExplorePage={true} />}
+
+      <div className="filter-container">
+        <div className="category-list-container explore-category-list">
+          {!isLoading && (
+            <Chips name={""} key={"all"}>
+              All
+            </Chips>
+          )}
+          {categories.map((ele) => (
+            <Chips name={ele.categoryName} key={ele._id}>
+              {ele.categoryName}
+            </Chips>
+          ))}
+        </div>
+
+        {!isLoading && (
+          <div className="sort-container">
+            <label htmlFor="sortBy">Sort </label>
+
+            <select
+              name="sortBy"
+              value={sortBy}
+              onChange={(e) =>
+                filtersDispatch({ type: SET_SORT, payload: e.target.value })
+              }
+            >
+              <option value="">-</option>
+              <option value="ascending">Ascending</option>
+              <option value="descending">Descending</option>
+            </select>
+          </div>
+        )}
+      </div>
+
+      {filteredVideos.length > 0 && (
         <div className="videos-container explore-container">
-          {videos.map((video) => (
+          {filteredVideos.map((video) => (
+            <VideoCard key={video._id} video={video} />
+          ))}
+          {/* {videos.map((video) => (
             <VideoCard key={video._id} video={video} />
           ))}
           {videos.map((video) => (
             <VideoCard key={video._id} video={video} />
-          ))}
-          {videos.map((video) => (
-            <VideoCard key={video._id} video={video} />
-          ))}
+          ))} */}
         </div>
       )}
     </div>
